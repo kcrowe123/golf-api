f07a8e4e5e110a4953f4826ea255320466c21460
Keith Crowe <kcrowe123@yahoo.com>
2015-11-24 20:47:05 -0600

    Checkstyle cleanup and added server.port to application.properties
    Checkstyle cleanup and added server.port to application.properties

--- src/main/java/com/golf/business/accessor/player/PlayerAccessor.java
+++ src/main/java/com/golf/business/accessor/player/PlayerAccessor.java
@@ -12,22 +12,40 @@ import com.golf.business.Filter;
 import com.golf.dao.player.PlayerDao;
 import com.golf.domain.player.Player;
 
-
+/**
+ * @author Keith  PlayerAccessor class.
+ */
 @Transactional
 @Service("playerAccessor")
 public class PlayerAccessor implements Accessor<Player> {
-	
-	@Autowired
-	PlayerDao playerDao;
+    
+    @Autowired
+    private PlayerDao playerDao;
 
-	@Override
-	public List<Player> find(Filter<Player> filter) {
-		return playerDao.getAll();
-	}
-	@Override
-	public Player load(Long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<Player> find(Filter<Player> filter) {
+        return playerDao.getAll();
+    }
+    @Override
+    public Player load(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    /**
+     * @return <code>PlayerDao</code>
+     */
+    public PlayerDao getPlayerDao() {
+        return playerDao;
+    }
+    
+    /**
+     * @param playerDao   <code>PlayerDao</code>
+     */
+    public void setPlayerDao(PlayerDao playerDao) {
+        this.playerDao = playerDao;
+    }
+    
+    
 
 }
--- src/main/java/com/golf/domain/player/Address.java
+++ src/main/java/com/golf/domain/player/Address.java
@@ -14,16 +14,16 @@ import javax.persistence.Transient;
 @Embeddable
 public class Address {
     
-	@Column(name="STREET", nullable = true)
+    @Column(name = "STREET", nullable = true)
     private String street;
     
-	@Column(name="CITY", nullable = true)
+    @Column(name = "CITY", nullable = true)
     private String city;
     
-	@Column(name="STATE", nullable = true)
+    @Column(name = "STATE", nullable = true)
     private String state;
     
-	@Column(name="ZIP_CODE", nullable = true)
+    @Column(name = "ZIP_CODE", nullable = true)
     private String zipCode;
 
 /**
@@ -32,49 +32,81 @@ public class Address {
     public Address() {
 
     }
-
+    
+    /**
+     * @param street  <code>String</code>
+     * @param city    <code>String</code>
+     * @param state   <code>String</code>
+     * @param zipCode <code>String</code>
+     */
     public Address( String street, String city, String state, String zipCode) {
         this.street = street;
         this.city = city;
         this.state = state;
         this.zipCode = zipCode;
     }
-
+    
+    /**
+     * @return <code>String</code>
+     */
     @Transient
-	public String getMailingAddress() {
-		return street + " " + city + ", " + state + " " + zipCode;
-	}
-
-	public String getStreet() {
-		return street;
-	}
-
-	public void setStreet(String street) {
-		this.street = street;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	@Column(name="ZIP_CODE", nullable = true)
-	public String getZipCode() {
-		return zipCode;
-	}
-
-	public void setZipCode(String zip) {
-		this.zipCode = zip;
-	}
+    public String getMailingAddress() {
+        return street + " " + city + ", " + state + " " + zipCode;
+    }
+    
+    /**
+     * @return <code>String</code>
+     */
+    public String getStreet() {
+        return street;
+    }
+    
+    /**
+     * @param street   <code>String</code>
+     */
+    public void setStreet(String street) {
+        this.street = street;
+    }
+    
+    /**
+     * @return <code>String</code>
+     */
+    public String getCity() {
+        return city;
+    }
+    
+    /**
+     * @param city  <code>String</code>
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+    
+    /**
+     * @return <code>String</code>
+     */
+    public String getState() {
+        return state;
+    }
+    
+    /**
+     * @param state  <code>String</code>
+     */
+    public void setState(String state) {
+        this.state = state;
+    }
+    
+    /**
+     * @return <code>String</code>
+     */
+    public String getZipCode() {
+        return zipCode;
+    }
+    
+    /**
+     * @param zip  <code>String</code>
+     */
+    public void setZipCode(String zip) {
+        this.zipCode = zip;
+    }
 }
